1b5f08215afec00a4d84af52e793f0dfa862753a
JairSB <benarrozjair@gmail.com>
2024-06-07 22:02:20 -0300

    agora com som

--- aula7do6/js/piano.js
+++ aula7do6/js/piano.js
@@ -1,7 +1,10 @@
 var tecla =document.querySelectorAll('button');
+var baru = document.getElementById('barulho')
 
 tecla.forEach(som =>{
     tecla.addEventListener('click',e=>{
         e.preventDefault();
+        baru.src ="notas/"+tecla.dataset.nota+".mp3";
+        baru.onplay();
     })
 });
